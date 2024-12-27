2e6ee84eb52cc511da8c71b5a5e9430c29cfc789
harveylount <h.lount6738@student.leedsbeckett.ac.uk>
2024-12-27 22:31:06 +0000

    Login function created

---
+++ loginForm.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<?php
+session_start();
+?>
+
+<head>
+
+    <link href="./index.css" rel="stylesheet" type="text/css" />
+
+    <title>Login Page</title>
+
+</head>
+
+<body>
+
+    <div id="pagewrap">
+
+        <header>
+
+            <h1>Name of Product</h1>
+
+            <h2> a Digital Forensics Case Management System </h2>
+
+        </header>
+
+        
+
+        <section id="content">
+
+            <h2>Login</h2>
+
+            <form method="post" action="loginInsert.php">
+                <fieldset class="field-set width">
+                    <legend>
+                    New User Details
+                    </legend>
+                    <label for="txtUsername">Username: </label><br />
+                    <input type="text" name="txtUsername" value="<?php 
+                        if(isset($_SESSION['txtUsernameF'])) {
+                            echo $_SESSION['txtUsernameF'];
+                            unset($_SESSION['txtUsernameF']);
+                        }
+                    ?>"/><?php echo $_SESSION['txtUsernameR']; unset($_SESSION['txtUsernameR']);?><br /><br />
+
+                    <label for="txtPassword">Password: </label><br />
+                    <input type="password" name="txtPassword" value="<?php 
+                        if(isset($_SESSION['txtPasswordF'])) {
+                            echo $_SESSION['txtPasswordF'];
+                            unset($_SESSION['txtPasswordF']);
+                        }
+                    ?>"/><?php echo $_SESSION['txtPasswordR']; unset($_SESSION['txtPasswordR']);?><br /><br />
+
+                    <input type="submit" value="Submit" name="subEvent" />
+                    <input type="reset" value="Clear" />
+                </fieldset>
+                
+            </form>
+            
+            <?php
+            echo $_SESSION['registered'];
+            unset($_SESSION['registered']);
+            ?>
+
+        </section>
+
+        
+
+        <footer>
+
+            <h4>Harvey Lount c3654483</h4>
+
+        </footer>
+
+    </div>
+
+</body>
+
+</html>
---
+++ loginInsert.php
@@ -0,0 +1,80 @@
+<?php
+include 'sqlConnection.php';
+
+$file = "encryptionkey.txt";
+if (file_exists($file)) {
+    $encryptionKey = file_get_contents($file);
+
+    if (isset($_POST['subEvent'])) {
+        $username=$_POST['txtUsername'];
+
+        $password=$_POST['txtPassword'];
+
+        if (!empty($username)) {
+            $_SESSION['txtUsernameF']=$_POST['txtUsername'];
+            
+            if (preg_match('/^[a-zA-Z]+$/', $username)) {
+                $usernameCheck = true;
+            } else {
+                $usernameCheck = false;
+                $_SESSION['txtUsernameR']=' Must only contain alpha characters';
+            }
+
+        } else {
+            $_SESSION['txtUsernameR']=' Username Required';
+        }
+
+        if (!empty($password)) {
+            $_SESSION['txtPasswordF']=$_POST['txtPassword'];
+            $passwordCheck = true;
+
+            if ($usernameCheck && $passwordCheck) {
+                
+                $encryptedPass = openssl_encrypt($password, 'aes-256-cbc', $encryptionKey, 0, $encryptionKey);
+                
+                $query="SELECT * FROM users WHERE Username='$username' AND UserPass='$encryptedPass'";
+                $result=mysqli_query($connection, $query);
+                
+
+                if ($row = mysqli_fetch_assoc($result)) {
+                    $_SESSION['userId']=$username;
+                    unset($_SESSION['txtUsernameF']);
+                    unset($_SESSION['txtPasswordF']);
+
+                    $query="SELECT role FROM users WHERE Username = '$username';";
+                    $result=mysqli_query($connection, $query);
+                    if ($result) {
+                        // Fetch the result as an associative array
+                        $row = mysqli_fetch_assoc($result);
+                    
+                        // Check if the row contains data
+                        if ($row) {
+                            // Store the role in the session
+                            $_SESSION['userRole'] = $row['role'];
+                    
+                        } else {
+                            // Handle case where no user is found
+                            header('location:loginForm.php');
+                        }
+                    }
+
+                    header('location:index.php');
+                    exit();
+
+                } else {
+                    $_SESSION['registered']='Login failed.';
+                    header('location:loginForm.php');
+                    exit();
+                }
+        
+            }
+
+        } else {
+            $_SESSION['txtPasswordR']=' Password Required';
+        }
+                            
+        header('location:loginForm.php');
+        $_SESSION['registered']='There were errors you have not been registered!';
+    }
+}
+?>
