b9be3256c543e45c0bafac2a4a8739ab1efbe573
harveylount <h.lount6738@student.leedsbeckett.ac.uk>
2024-12-27 20:30:24 +0000

    Created index.php file

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <link href="./index.css" rel="stylesheet" type="text/css" />
+
+    <title>WAT Portfolio</title>
+
+</head>
+
+<body>
+
+    <div id="pagewrap">
+
+        <header>
+
+            <h1>DFCMS</h1>
+
+            <h2> a Digital Forensics Case Management System </h2>
+
+        </header>
+
+        
+
+
+        <section id="content">
+
+            <h2>Cases</h2>
+
+            <p>Cases displayed here</p>
+
+        </section>
+
+        
+
+        <footer>
+
+            <h4>Harvey Lount c3654483</h4>
+
+        </footer>
+
+    </div>
+
+</body>
+
+</html>
