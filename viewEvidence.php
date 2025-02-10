b32d5bd645d814e91565f8aa46df51910505cbb8
harveylount <h.lount6738@student.leedsbeckett.ac.uk>
2025-02-10 13:08:18 +0000

    Created view evidence page

---
+++ viewEvidence.php
@@ -0,0 +1,65 @@
+<?php
+include 'sqlConnection.php'; 
+if(!isset($_SESSION['userId'])){
+    header ('location:loginForm.php');
+}
+
+$identifier = intval($_GET['identifier']);  // Sanitize the input to prevent SQL injection
+?> 
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <link href="./index.css" rel="stylesheet" type="text/css" />
+
+    <title>Evidence</title>
+
+</head>
+
+<body>
+
+    <div id="pagewrap">
+
+        <div id="logout-bar">
+            <span id="username">Username: <?php echo $_SESSION['userId']; ?></span>
+            <span id="role">Role: <?php echo $_SESSION['userRole']; ?></span>
+            <a href="logoutFunction.php" id="logout-button">Logout</a>
+        </div>
+
+        <header>
+
+            <h1>DFCMS</h1>
+
+            <h2> a Digital Forensics Case Management System </h2>
+
+        </header>
+
+        <div id="navcase-bar">
+            <a href="<?php echo "viewCase.php?identifier=$identifier" ?>" id="navcase-button">Case Overview</a>
+            <a href="<?php echo "viewEvidence.php?identifier=$identifier" ?>" id="navcase-button">Evidence</a>
+        </div>
+
+        <section id="content">
+
+            <p>
+                Evidence here
+            </p>
+
+        </section>
+
+        
+
+        <footer>
+
+            <h4>Harvey Lount c3654483</h4>
+
+        </footer>
+
+    </div>
+
+</body>
+
+</html>
