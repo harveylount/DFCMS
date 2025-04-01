c7757fbd645e2145c3165c03c1beabdac5385e2b
harveylount <h.lount6738@student.leedsbeckett.ac.uk>
2025-04-01 17:53:24 +0100

    Created view LBU02 form graphical interface

---
+++ viewLBU02.php
@@ -0,0 +1,164 @@
+<?php
+include 'sqlConnection.php'; 
+if(!isset($_SESSION['userId'])){
+    header ('location:loginForm.php');
+}
+
+$identifier = intval($_GET['identifier']);  // Sanitize the input to prevent SQL injection
+$evidenceID = intval($_GET['EvidenceID']);  // Sanitize the input to prevent SQL injection
+?> 
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <link href="./index.css" rel="stylesheet" type="text/css" />
+
+    <title>LBU02</title>
+
+</head>
+
+<body>
+
+    <div id="pagewrap">
+
+        <div id="logout-bar">
+            <span id="username">Username: <?php echo $_SESSION['userId']; ?></span>
+            <span id="role">Role: <?php echo $_SESSION['userRole']; ?></span>
+            <a href="logoutFunction.php" id="logout-button">Logout</a>
+        </div>
+
+        <header>
+
+            <h1>DFCMS</h1>
+
+            <h2> a Digital Forensics Case Management System </h2>
+
+        </header>
+
+        <div id="navcase-bar">
+            <a href="<?php echo "viewEvidenceExhibit.php?identifier=$identifier&EvidenceID=$evidenceID" ?>" id="navcase-button">Evidence Overview</a>
+            <a href="<?php echo "viewLBU01.php?identifier=$identifier&EvidenceID=$evidenceID" ?>" id="navcase-button">LBU01</a>
+            <a href="<?php echo "viewLBU02.php?identifier=$identifier&EvidenceID=$evidenceID" ?>" id="navcase-button">LBU02</a>
+            <a href="<?php echo "viewLBU03.php?identifier=$identifier&EvidenceID=$evidenceID" ?>" id="navcase-button">LBU03</a>
+            <?php include 'lbu04notComputerFunction.php'; ?>
+            <a href="<?php echo "viewLBU05.php?identifier=$identifier&EvidenceID=$evidenceID" ?>" id="navcase-button">LBU05</a>
+            <a href="<?php echo "viewCrimeSceneReports.php?identifier=$identifier"?>" id="navcase-button">LBU06</a>
+        </div>
+
+        <section id="LBU">
+
+            <p>
+                <?php
+                $query = "SELECT * FROM lbu02 WHERE Identifier = ? AND EvidenceID = ?";
+                $stmt = $connection->prepare($query);
+                $stmt->bind_param("ss", $identifier, $evidenceID);  
+                $stmt->execute();
+                $results = $stmt->get_result();
+
+                while ($row = mysqli_fetch_assoc($results)) {
+                    if ($row['ExternalBoolean'] === "true") {
+
+                        echo "<table cellpadding='10' cellspacing='0' style='width: 100%; border-collapse: collapse; border: 2px solid #5AAAFF;'>"; 
+                        echo "<tr><td rowspan='2' style='font-size: 50px; font-weight: bold; border: 2px solid #5AAAFF; background-color: #5AAAFF; color: white;'>DFCMS</td> 
+                                <td style='text-align: right; border: 2px solid #5AAAFF; background-color: #5AAAFF; color: white; font-weight: bold; font-size: 20px;'>" . 'LBU02 - Exhibit Dispatch Form' . "</td></tr>"; 
+                        echo "<tr><td style='text-align: right; border: 2px solid #5AAAFF; background-color: #5AAAFF; color: white; font-weight: bold; font-size: 20px;'>" . 'Page 1 of 1' . "</td></tr>";
+                        echo "</table>";
+                        echo "<br/>";
+                        
+                        echo "<table class='styled-table' border='1' cellpadding='10' cellspacing='0' style='width: 100%;'>";
+                        echo "<tr><td class='lbu-high'>External Email Recipient</td><td>" . $row['ExternalEmail'] . "</td></tr>";
+                        echo "<tr><td class='lbu-high'>Timestamp Sent</td><td>" . $row['EmailTimestamp'] . "</td></tr>";
+                        echo "<tr><td class='lbu-high'>PDF MD5 Hash</td><td>" . $row['PDFMD5Hash'] . "</td></tr>";
+                        echo "<tr><td class='lbu-high'>PDF SHA-1 Hash</td><td>" . $row['PDFSHA1Hash'] . "</td></tr>";
+                        echo "</table>";
+                        echo "<br/>";
+                        
+                        echo "<table class='styled-table' border='1' cellpadding='10' cellspacing='0' style='width: 100%;'>";
+                        echo "<tr><td class='lbu-high'>Case Reference</td><td>" . $row['CaseReference'] . "</td></tr>";
+                        echo "<tr><td class='lbu-high'>Location</td><td>" . $row['Location'] . "</td></tr>";
+                        echo "</table>";
+                        echo "<br/>";
+
+                        echo "<table class='styled-table' border='1' cellpadding='10' cellspacing='0' style='width: 100%;'>";
+                        echo "<tr><td class='lbu-high'>Dispatched By Rank</td><td>" . $row['DispatchedByRank'] . "</td></tr>";
+                        echo "<tr><td class='lbu-high'>Dispatched By Name</td><td>" . $row['DispatchedByName'] . "</td></tr>";
+                        echo "<tr><td class='lbu-high'>Dispatched By Company</td><td>" . $row['DispatchedByCompany'] . "</td></tr>";
+                        echo "<tr><td class='lbu-high'>Dispatched By Timestamp</td><td>" . $row['DispatchedByTime'] . "</td></tr>";
+                        echo "<tr><td class='lbu-high'>Dispatched By Signature</td><td><img src='" . $row['DispatchedBySig'] . "' alt='Signature'></td></tr>";
+                        echo "</table>";
+                        echo "<br/>";
+
+                        echo "<table class='styled-table' border='1' cellpadding='10' cellspacing='0' style='width: 100%;'>";
+                        echo "<tr><td class='lbu-high'>Received By Rank</td><td>" . $row['ReceivedByRank'] . "</td></tr>";
+                        echo "<tr><td class='lbu-high'>Received By Name</td><td>" . $row['ReceivedByName'] . "</td></tr>";
+                        echo "<tr><td class='lbu-high'>Received By Company</td><td>" . $row['ReceivedByCompany'] . "</td></tr>";
+                        echo "<tr><td class='lbu-high'>Received By Timestamp</td><td>" . $row['ReceivedByTime'] . "</td></tr>";
+                        echo "<tr><td class='lbu-high'>Received By Signature</td><td><img src='" . $row['ReceivedBySig'] . "' alt='Signature'></td></tr>";
+                        echo "</table>";
+                        echo "<br/>";
+
+                        echo "<table class='styled-table' border='1' cellpadding='10' cellspacing='0' style='width: 100%;'>";
+                        echo "<tr><td class='lbu-high'>Exhibit Number</td><td>" . $row['ExhibitRef'] . "</td></tr>";
+                        echo "<tr><td class='lbu-high'>Seal Number</td><td>" . $row['InitialSealNumber'] . "</td></tr>";
+
+                        echo "<tr><th class='lbu-dark' colspan='2'>Initial Description</th></tr>";
+                        echo "<tr><td colspan='2'>" . $row['InitialDescription'] . "</td></tr>";
+                        echo "</table>";
+                    } else {
+                        echo "<h2>" . $row['ExhibitRef'] . " - Exhibit Receipt Form (LBU02)</h2>";
+                        
+                        echo "<table class='styled-table' border='1' cellpadding='10' cellspacing='0' style='width: 100%;'>";
+                        echo "<tr><td class='lbu-high'>Case Reference</td><td>" . $row['CaseReference'] . "</td></tr>";
+                        echo "<tr><td class='lbu-high'>Location</td><td>" . $row['Location'] . "</td></tr>";
+                        echo "</table>";
+                        echo "<br/>";
+
+                        echo "<table class='styled-table' border='1' cellpadding='10' cellspacing='0' style='width: 100%;'>";
+                        echo "<tr><td class='lbu-high'>Dispatched By Rank</td><td>" . $row['DispatchedByRank'] . "</td></tr>";
+                        echo "<tr><td class='lbu-high'>Dispatched By Name</td><td>" . $row['DispatchedByName'] . "</td></tr>";
+                        echo "<tr><td class='lbu-high'>Dispatched By Company</td><td>" . $row['DispatchedByCompany'] . "</td></tr>";
+                        echo "<tr><td class='lbu-high'>Dispatched By Timestamp</td><td>" . $row['DispatchedByTime'] . "</td></tr>";
+                        echo "<tr><td class='lbu-high'>Dispatched By Signature</td><td><img src='" . $row['DispatchedBySig'] . "' alt='Signature'></td></tr>";
+                        echo "</table>";
+                        echo "<br/>";
+
+                        echo "<table class='styled-table' border='1' cellpadding='10' cellspacing='0' style='width: 100%;'>";
+                        echo "<tr><td class='lbu-high'>Received By Rank</td><td>" . $row['ReceivedByRank'] . "</td></tr>";
+                        echo "<tr><td class='lbu-high'Received By Name</td><td>" . $row['ReceivedByName'] . "</td></tr>";
+                        echo "<tr><td class='lbu-high'>Received By Company</td><td>" . $row['ReceivedByCompany'] . "</td></tr>";
+                        echo "<tr><td class='lbu-high'>Received By Timestamp</td><td>" . $row['ReceivedByTime'] . "</td></tr>";
+                        echo "<tr><td class='lbu-high'>Received By Signature</td><td><img src='" . $row['ReceivedBySig'] . "' alt='Signature'></td></tr>";
+                        echo "</table>";
+                        echo "<br/>";
+
+                        echo "<table class='styled-table' border='1' cellpadding='10' cellspacing='0' style='width: 100%;'>";
+                        echo "<tr><td class='lbu-high'>Exhibit Number</td><td>" . $row['ExhibitRef'] . "</td></tr>";
+                        echo "<tr><td class='lbu-high'>Seal Number</td><td>" . $row['InitialSealNumber'] . "</td></tr>";
+
+                        echo "<tr><th class='lbu-dark' colspan='2'>Initial Description</th></tr>";
+                        echo "<tr><td colspan='2'>" . $row['InitialDescription'] . "</td></tr>";
+                        echo "</table>";
+                    }
+                }
+                $stmt->close();
+                ?>
+            </p>
+
+        </section>
+
+        
+
+        <footer>
+
+            <h4>Harvey Lount c3654483</h4>
+
+        </footer>
+
+    </div>
+
+</body>
+
+</html>
