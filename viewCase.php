080f9250aa161caa8238809aebb932522f485c9e
harveylount <h.lount6738@student.leedsbeckett.ac.uk>
2025-04-18 17:38:36 +0100

    Report Generation: Added 'Reports' button to relevant pages

--- viewCase.php
+++ viewCase.php
@@ -75,6 +75,7 @@ mysqli_stmt_close($stmt);
             <a href="<?php echo "viewEvidence.php?identifier=$identifier" ?>" id="navcase-button">Evidence</a>
             <a href="<?php echo "viewCrimeSceneReports.php?identifier=$identifier" ?>" id="navcase-button">Crime Scene Reports</a>
             <a href="<?php echo "viewCaseNotes.php?identifier=$identifier" ?>" id="navcase-button">Case Notes</a>
+            <a href="<?php echo "listReports.php?identifier=$identifier" ?>" id="navcase-button">Reports</a>
             <?php include 'displayCaseAdminButtonFunction.php'; ?>
         </div>
 
